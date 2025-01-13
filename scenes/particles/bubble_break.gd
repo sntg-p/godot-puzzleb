e1c05c27cf41e2b36ca4d1ab2b61f06d393ef7f5
Santiago Podestá <santiagolp98@gmail.com>
2025-01-13 20:34:10 -0300

    better colors in bubble break particles

--- scenes/particles/bubble_break.gd
+++ scenes/particles/bubble_break.gd
@@ -1,21 +1,22 @@
 class_name BubbleBreakParticles extends GPUParticles2D
 
 
-static var colors = [
+@export var colors = [
 	Color.RED,
-	Color.YELLOW,
-	Color.BLUE,
-	Color.GREEN,
-	Color.GRAY,
-	Color.PURPLE,
-	Color.ORANGE,
+	Color.from_hsv(.15, 1, 1),
+	Color.from_hsv(.61, 1, 1),
+	Color.from_hsv(.33, 1, 1),
+	Color.from_hsv(0, 0, .8),
+	Color.from_hsv(.77, 1, 1),
+	Color.from_hsv(.08, 1, 1),
 ]
 
 
 var type: int:
 	set(value):
 		var color = colors[value]
-		self.process_material.color = color
+		var material: ShaderMaterial = self.material
+		material.set_shader_parameter("color", color)
 		self.emitting = true
 
 
